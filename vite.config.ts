6c9d8494b1812463521eb2fed303278ce1f63087
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-02-03 14:07:03 +0100

    chore: new changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,18 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, "src/app.js"),
+        style: resolve(__dirname, "src/style.css"),
+
+        main: resolve(__dirname, "index.html"),
+        contact: resolve(__dirname, "contact.html"),
+      },
+    },
+  },
 });
